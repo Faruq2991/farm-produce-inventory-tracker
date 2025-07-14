ce739ac61e0b67dca166b5413a4efd8465d6ddba
faruq2991 <faruqadam.92@gmail.com>
2025-07-14 01:59:30 +0000

    feat: created inventory.py to implement core inventory functionalities.

--- app/models/inventory.py
+++ app/models/inventory.py
@@ -5,16 +5,14 @@ class Inventory():
     def __init__(self):
         self.produces = []
         self.total_revenue = 0.0
-        return
 
     def add_item(self, name: str, quantity: int, price: float):
-        produce = ProduceItem(name, quantity, price)
-        self.produces.append(produce)
 
         for item in self.produces:
             if item.name.lower() == item.lower():
-                produce.update_quantity(item.quantity + quantity)
-                produce.update_price(price)
+                new_quantity = item.quantity + quantity
+                item.update_quantity(new_quantity)
+                item.update_price(price)
                 print("Updated existing item.")
                 return
 
@@ -30,8 +28,20 @@ class Inventory():
                 print(item)
 
     def record_sale(self, name: str, quantity_sold: int):
+        for item in self.produces:
+            if item.name.lower() == name.lower():
+                if quantity_sold > item.quantity:
+                    print("Not enough stock available.")
+                    return
+                new_quantity = item.quantity - quantity_sold
+                item.update_quantity(new_quantity)
+                sale_amount = quantity_sold * item.price_per_unit
+                self.total_revenue += sale_amount
 
+                print("{quantity_sold} {item.name} sold for ${sale_amount:.2f}")
+                return
+        print("Item not found in inventory.")
         return
 
     def get_total_revenue(self):
-        return
+        return self.total_revenue
